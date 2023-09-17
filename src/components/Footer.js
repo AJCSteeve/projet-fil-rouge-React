51c492e4c02161b586446b75aa1acd031986b1ca
Steeve Lowenski <steeve.lowenski@gmail.com>
2023-09-17 16:51:05 +0200

    fix Footer for merge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,16 @@ import {Link} from "react-router-dom";
 function Footer() {
     return (
         <>
-            <table id="temporary-footer-nav">
-                <tr>
-                    <td><h2>Footer</h2></td>
-                    <td><p>Temporary links : </p></td>
-                    <td><a href="register">register</a></td>
-                    <td><a href="login">login</a></td>
-                </tr>
-            </table>
+            {/*<table id="temporary-footer-nav">*/}
+            {/*    <tr>*/}
+            <h2>Footer</h2>
+
+            {/*        <td><h2>Footer</h2></td>*/}
+                    {/*<td><p>Temporary links : </p></td>*/}
+            {/*        <td><a href="register">register</a></td>*/}
+            {/*        <td><a href="login">login</a></td>*/}
+            {/*    </tr>*/}
+            {/*</table>*/}
         </>
     );
 }
