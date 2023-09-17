ff4a192b78321be5a21d1659246f51a833a6cbae
NicolasBourassin <bourassinnicolas@gmail.com>
2023-09-17 11:54:50 +0200

    various tests with css following back-office visuals. Cannot yet display background image like back-office.

--- src/App.js
+++ src/App.js
@@ -14,36 +14,39 @@ import TransactionRating from "./components/transaction/TransactionRating";
 import CheckSellingHistory from "./components/transaction/CheckSellingHistory";
 import CheckPurchaseHistory from "./components/transaction/CheckPurchaseHistory";
 
+
 function App() {
     return (
         <div className="App">
-            <><Header/></>
-            <Routes>
-                {/* Routes about user account :
+            <div className='my-card-bg'><Header/></div>
+            <div className='my-custom-bg'>
+                <Routes>
+                    {/* Routes about user account :
                 - register
                 - login
                 - update user profile
                 - verify user profile //TODO DEFINIR VERIFICATION DU PROFIL
                 */}
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/profile/update" element={<UpdateProfile/>}/>
-                <Route path="/profile/verify" element={<VerifyProfile/>}/>
-                {/* Routes about transactions :
+                    <Route path="/register" element={<Register/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/profile/update" element={<UpdateProfile/>}/>
+                    <Route path="/profile/verify" element={<VerifyProfile/>}/>
+                    {/* Routes about transactions :
                 - search, selling, purchase of tickets,
                 - rating of a transaction,
                 - consulting transaction history (sellings & purchases)
                 */}
-                <Route path="/tickets/search" element={<SearchTickets/>}/>
-                <Route path="/tickets/selling" element={<SellingTickets/>}/>
-                <Route path="/tickets/purchase" element={<PurchaseTickets/>}/>
-                <Route path="/transaction/rating" element={<TransactionRating/>}/>
-                <Route path="/transaction/history/selling" element={<CheckSellingHistory/>}/>
-                <Route path="/transaction/history/purchase" element={<CheckPurchaseHistory/>}/>
-
-            </Routes>
-
-            <><Footer/></>
+                    <Route path="/tickets/search" element={<SearchTickets/>}/>
+                    <Route path="/tickets/selling" element={<SellingTickets/>}/>
+                    <Route path="/tickets/purchase" element={<PurchaseTickets/>}/>
+                    <Route path="/transaction/rating" element={<TransactionRating/>}/>
+                    <Route path="/transaction/history/selling" element={<CheckSellingHistory/>}/>
+                    <Route path="/transaction/history/purchase" element={<CheckPurchaseHistory/>}/>
+                </Routes>
+                {/*fixme the background image isn't displayed, while the picture URL is right.*/}
+                <div className='bg-image p-5 text-center shadow-1-strong rounded mb-5 text-light'></div>
+            </div>
+            <div className='footer'><Footer/></div>
         </div>
     );
 }
