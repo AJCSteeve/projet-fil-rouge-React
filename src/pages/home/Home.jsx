e61500f198748cd6a48ab2b7e5036e618551d4d7
Steeve Lowenski <steeve.lowenski@gmail.com>
2023-09-19 16:18:40 +0200

    home modif

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,6 @@ import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/Header";
 import Events from "../../components/events/Events";
 import MailList from "../../components/mailList/MailList";
-import Footer from "../../components/footer/Footer";
 
 const Home = () => {
     return (
